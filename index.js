f3360adb69ce99cce5950504627bad9b837db6d5
rinchz <rinchainz@gmail.com>
2026-03-03 17:43:22 +0500

    Пункт 2: добавлен show

--- index.js
+++ index.js
@@ -16,10 +16,27 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            // Пункт 2: Получаем и выводим все TODO
+            const todos = getAllTodos();
+            todos.forEach(todo => console.log(todo));
+            break;
         default:
             console.log('wrong command');
             break;
     }
 }
 
-// TODO you can do it!
+// Функция для поиска всех TODO в файлах
+function getAllTodos() {
+    const todos = [];
+    const todoRegex = /\/\/ TODO.*/g; // Регулярное выражение для поиска // TODO и всего текста после него
+
+    for (const fileContent of files) {
+        const matches = fileContent.match(todoRegex);
+        if (matches) {
+            todos.push(...matches);
+        }
+    }
+    return todos;
+}
