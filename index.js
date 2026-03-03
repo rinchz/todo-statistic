afa72be33fe5a665b3971d32009ed959f02503c9
Cumparator <vashch153@gmail.com>
2026-03-03 17:49:53 +0500

    базовый минимум

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
+const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
+const { readLine } = require('./console');
 
 const files = getFiles();
 
@@ -12,14 +12,30 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    const [action, ...args] = command.split(' ');
+    const arg = args.join(' ');
+    const todos = getAllTodos().map(parseTodo);
+
+    switch (action) {
         case 'exit':
             process.exit(0);
             break;
         case 'show':
-            // Пункт 2: Получаем и выводим все TODO
-            const todos = getAllTodos();
-            todos.forEach(todo => console.log(todo));
+            todos.forEach(t => console.log(t.raw));
+            break;
+        case 'important':
+            todos
+                .filter(t => t.importance > 0)
+                .forEach(t => console.log(t.raw));
+            break;
+        case 'user':
+            if (!arg) break;
+            todos
+                .filter(t => t.user === arg.toLowerCase())
+                .forEach(t => console.log(t.raw));
+            break;
+        case 'sort':
+            sortTodos(todos, arg).forEach(t => console.log(t.raw));
             break;
         default:
             console.log('wrong command');
@@ -27,10 +43,9 @@ function processCommand(command) {
     }
 }
 
-// Функция для поиска всех TODO в файлах
 function getAllTodos() {
     const todos = [];
-    const todoRegex = /\/\/ TODO.*/g; // Регулярное выражение для поиска // TODO и всего текста после него
+    const todoRegex = /\/\/ TODO.*/g;
 
     for (const fileContent of files) {
         const matches = fileContent.match(todoRegex);
@@ -39,4 +54,40 @@ function getAllTodos() {
         }
     }
     return todos;
+}
+
+function parseTodo(rawStr) {
+    const content = rawStr.replace('// TODO ', '');
+    const parts = content.split(';').map(p => p.trim());
+    const isFullFormat = parts.length === 3;
+
+    return {
+        raw: rawStr,
+        user: isFullFormat ? parts[0].toLowerCase() : null,
+        date: isFullFormat ? new Date(parts[1]) : null,
+        importance: (rawStr.match(/!/g) || []).length,
+        comment: isFullFormat ? parts[2] : content
+    };
+}
+
+function sortTodos(todos, type) {
+    const result = [...todos];
+    switch (type) {
+        case 'importance':
+            return result.sort((a, b) => b.importance - a.importance);
+        case 'user':
+            return result.sort((a, b) => {
+                const nameA = a.user || '{'; 
+                const nameB = b.user || '{';
+                return nameA.localeCompare(nameB);
+            });
+        case 'date':
+            return result.sort((a, b) => {
+                const dateA = a.date || 0;
+                const dateB = b.date || 0;
+                return dateB - dateA;
+            });
+        default:
+            return result;
+    }
 }
